95254f60934fa8026d0112d5e464d51f985011eb
Taha <mt190502@mtaha.dev>
2024-09-16 18:05:50 +0300

    feat: added auto chart updater

---
+++ .github/updater.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+import re
+import requests
+import os
+import yaml
+
+current_directory = os.getcwd()
+repository = os.environ['GITHUB_REPOSITORY']
+
+
+def compare_versions(current, latest):
+    if current['maj'] < latest['maj']:
+        return 'upgrade-major'
+    elif current['maj'] > latest['maj']:
+        return 'downgrade-major'
+    elif (current['min'] < latest['min']) or (current['min'] == latest['min'] and current['pat'] < latest['pat']):
+        return 'upgrade'
+    elif (current['min'] > latest['min']) or (current['min'] == latest['min'] and current['pat'] > latest['pat']):
+        return 'downgrade'
+    else:
+        return 'no change'
+
+
+def parse_version(version):
+    parts = version.split('.')
+    return {
+        'maj': int(parts[0]),
+        'min': int(parts[1]),
+        'pat': parts[2] if len(parts) > 2 else 0,
+        'ver': version,
+        'nodot': version.replace('.', '')
+    }
+
+
+def get_chart_datas():
+    chart_data = []
+    chart_folder = current_directory + "/charts"
+    charts = os.listdir(chart_folder)
+
+    for chart in charts:
+        chart_file = chart_folder + f"/{chart}/Chart.yaml"
+        if not os.path.exists(chart_file):
+            print(f"Chart file not found in '{chart}' folder, skipping")
+            continue
+        with open(chart_file, "r") as data:
+            chart_data.append({"app": chart, "file": chart_file, "data": yaml.safe_load(data)})
+    return chart_data
+
+
+def update_version():
+    charts = get_chart_datas()
+    for chart in charts:
+        chart_data = chart.get('data')
+
+        image = chart_data.get('projectUrl').split("github.com/")[1]
+        chart_app_name = chart_data.get('name')
+        chart_app_version = parse_version(str(chart_data.get('appVersion')))
+        chart_version     = parse_version(str(chart_data.get('version')))
+   
+        response = requests.get(f'https://api.github.com/repos/{image}/tags')
+        latest_version = parse_version(re.sub(r'[a-zA-Z]', '', [tag['name'] for tag in response.json()][0]))
+        version_status = compare_versions(chart_app_version, latest_version)
+
+        if version_status == 'upgrade-major':
+            print(f"App '{chart_app_name}' needs an major-upgrade. ({chart_app_version['ver']} -> {latest_version['ver']})")
+            chart_data['majorUpgrade'] = True
+        elif version_status == 'downgrade-major':
+            print(f"App '{chart_app_name}' needs an major-downgrade. ({chart_app_version['ver']} -> {latest_version['ver']})")
+            chart_data['majorDowngrade'] = True
+        elif version_status == 'upgrade':
+            print(f"App '{chart_app_name}' needs an upgrade. ({chart_app_version['ver']} -> {latest_version['ver']})")
+        elif version_status == 'downgrade':
+            print(f"App '{chart_app_name}' needs an downgrade. ({chart_app_version['ver']} -> {latest_version['ver']})")
+        else:
+            print(f"App '{chart_app_name}' is up-to-date.")
+            continue
+
+        with open(chart.get('file'), 'w') as f:
+            chart_data['appVersion'] = latest_version['ver']
+            chart_data['version'] = latest_version['ver']
+            yaml.dump(chart_data, f, default_flow_style=False, sort_keys=False, width=float("inf"))
+
+
+if __name__ == '__main__':
+    if current_directory.split("/")[-1] != repository.split("/")[-1]:
+        print(f"""Current direcory is not '{repository.split("/")[-1]}', aborting...\n(current directory: {current_directory})""")
+        exit(1)
+    update_version()
