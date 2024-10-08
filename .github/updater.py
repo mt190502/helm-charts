537a1e1f6dc85fa63ad93e292fa3c58f285634f0
Taha <mt190502@mtaha.dev>
2024-10-08 13:03:00 +0300

    refactor: improve version comparison logic and update chart version

--- .github/updater.py
+++ .github/updater.py
@@ -15,10 +15,10 @@ def compare_versions(current, latest):
         return 'upgrade-major'
     elif current['maj'] > latest['maj']:
         return 'downgrade-major'
-    elif (current['min'] < latest['min']) or (current['min'] == latest['min'] and current['pat'] < latest['pat']):
-        return 'upgrade'
-    elif (current['min'] > latest['min']) or (current['min'] == latest['min'] and current['pat'] > latest['pat']):
-        return 'downgrade'
+    elif ((current['min'] < latest['min']) or (current['min'] > latest['min'])):
+        return 'minor'
+    elif ((current['pat'] < latest['pat']) or (current['pat'] > latest['pat'])):
+        return 'patch'
     else:
         return 'no change'
 
@@ -75,6 +75,7 @@ def update_version():
 
         image = chart_data.get('projectUrl').split("github.com/")[1]
         chart_app_version = parse_version(str(chart_data.get('appVersion')))
+        chart_version = parse_version(str(chart_data.get('version')))
   
         response = requests.get(f'https://api.github.com/repos/{image}/tags')
         versions = filter_valid_versions([re.sub(r'[a-zA-Z]', '', tag['name']) for tag in response.json()])
@@ -82,23 +83,29 @@ def update_version():
         latest_version = parse_version(versions[0])
         version_status = compare_versions(chart_app_version, latest_version)
 
-        if version_status == 'upgrade-major':
-            print(f"App '{chart_app_name}' needs an major-upgrade. ({chart_app_version['ver']} -> {latest_version['ver']})")
-            chart_data['majorUpgrade'] = True
-        elif version_status == 'downgrade-major':
-            print(f"App '{chart_app_name}' needs an major-downgrade. ({chart_app_version['ver']} -> {latest_version['ver']})")
-            chart_data['majorDowngrade'] = True
-        elif version_status == 'upgrade':
-            print(f"App '{chart_app_name}' needs an upgrade. ({chart_app_version['ver']} -> {latest_version['ver']})")
-        elif version_status == 'downgrade':
-            print(f"App '{chart_app_name}' needs an downgrade. ({chart_app_version['ver']} -> {latest_version['ver']})")
-        else:
-            print(f"App '{chart_app_name}' is up-to-date.")
-            continue
+        match (version_status):
+            case 'upgrade-major':
+                print(f"App '{chart_app_name}' needs an major-upgrade. ({chart_app_version['ver']} -> {latest_version['ver']})")
+                chart_version['maj'] += 1
+                chart_version['min'] = 0
+                chart_version['pat'] = str(int(chart_version['pat']) + 1)
+                chart_data['majorUpgrade'] = True
+            case 'downgrade-major':
+                print(f"App '{chart_app_name}' needs an major-downgrade. ({chart_app_version['ver']} -> {latest_version['ver']})")
+                chart_version['pat'] = str(int(chart_version['pat']) + 1)
+                chart_data['majorDowngrade'] = True
+            case 'minor':
+                chart_version['min'] += 1
+                chart_version['pat'] = str(0)
+            case 'patch':
+                chart_version['pat'] = str(int(chart_version['pat']) + 1)
+            case _:
+                print(f"App '{chart_app_name}' is up-to-date.")
+                continue
 
         with open(chart.get('file'), 'w') as f:
             chart_data['appVersion'] = latest_version['ver']
-            chart_data['version'] = latest_version['ver']
+            chart_data['version'] = f"{chart_version['maj']}.{chart_version['min']}.{chart_version['pat']}"
             yaml.dump(chart_data, f, default_flow_style=False, sort_keys=False, width=float("inf"))
 
 
